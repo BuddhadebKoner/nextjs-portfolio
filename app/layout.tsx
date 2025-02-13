ce8c218a97feffaa52b275d8a8cae6eeaf756797
BuddhadebKoner <jeetkoner36@gmail.com>
2025-02-13 21:37:10 +0530

    fab icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
+  icons: "/favicon.ico",
   title: "Buddhadeb Koner | Portfolio",
   description: "Buddhadeb Koner's Portfolio",
 };
